d341e328ca80ad11ac815846c1035ca33efabade
Jackson K <jacksonkonjengbam@gmail.com>
2022-11-13 14:02:44 +0530

    Adding form for /views/user/forgotpassword file

--- views/user/forgotpassword.php
+++ views/user/forgotpassword.php
@@ -6,7 +6,14 @@
 use yii\bootstrap4\ActiveForm;
 use yii\helpers\Url;
 
+$this->registerCssFile("@web/css/auth/forgotpassword.css", ['depends' => \app\assets\AuthAsset::class]);
+$this->registerJsFile("@web/js/auth/forgotpassword.js", ['depends' => \app\assets\AuthAsset::class]);
+
 $this->title = 'Forgot Password';
 ?>
 
 <h1>Forgot password</h1>
+
+<?php $form = ActiveForm::begin() ?>
+
+<?php ActiveForm::end() ?>
