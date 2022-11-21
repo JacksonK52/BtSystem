619eb51343c2236c45d7fdedb5842a8be6f42f7f
Jackson Konjengbam <jacksonkonjengbam@gmail.com>
2022-11-21 18:28:49 +0530

    Adding Email Variable

---
+++ config/email.php
@@ -0,0 +1,11 @@
+<?php
+
+// Mailtrap Setup
+return [
+    'class' => 'Swift_SmtpTransport',
+    'host' => 'smtp.mailtrap.io',
+    'username' => '',
+    'password' => '',
+    'port' => '',
+    'encryption' => '',
+];
